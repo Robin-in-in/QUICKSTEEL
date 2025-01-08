79dab9be0eb8f7cba88da45331baf52ce8b2fa3d
Tyson Line <123592329+TysonLine@users.noreply.github.com>
2025-01-08 01:57:44 -0500

    offset

--- ONESHOT_1/index.js
+++ ONESHOT_1/index.js
@@ -6,7 +6,7 @@ const c = canvas.getContext('2d')
 canvas.width = 1024
 canvas.height = 576
 
-const canvasOffset = 200
+const canvasOffset = 500
 
 c.fillRect(0,0, canvas.width, canvas.height)
 //0,0 is topleft
